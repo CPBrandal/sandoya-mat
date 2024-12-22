df84ad05d60a09454a5e6181db9c67701650269b
Peter Andreas Bertelsen Brandal <pabranda@ntnu.stud.no>
2024-12-22 22:21:56 +0100

    djoad

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
      <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Sandøya Mat</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
